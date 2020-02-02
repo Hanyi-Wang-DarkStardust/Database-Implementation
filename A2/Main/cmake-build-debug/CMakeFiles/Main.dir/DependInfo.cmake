054720d8b6532d3999c34f4fa81c199f30115ae8
Hanyi Wang <hw48@rice.edu>
2020-02-02 16:39:33 -0600

    Initialize

---
+++ A2/Main/cmake-build-debug/CMakeFiles/Main.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/BufferMgr/source/MyDB_BufferManager.cc" "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/cmake-build-debug/CMakeFiles/Main.dir/BufferMgr/source/MyDB_BufferManager.cc.o"
+  "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/BufferMgr/source/MyDB_Page.cc" "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/cmake-build-debug/CMakeFiles/Main.dir/BufferMgr/source/MyDB_Page.cc.o"
+  "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/BufferTest/source/BufferQUnit.cc" "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/cmake-build-debug/CMakeFiles/Main.dir/BufferTest/source/BufferQUnit.cc.o"
+  "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/Catalog/source/MyDB_Catalog.cc" "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/cmake-build-debug/CMakeFiles/Main.dir/Catalog/source/MyDB_Catalog.cc.o"
+  "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/Catalog/source/MyDB_Schema.cc" "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/cmake-build-debug/CMakeFiles/Main.dir/Catalog/source/MyDB_Schema.cc.o"
+  "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/Catalog/source/MyDB_Table.cc" "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/cmake-build-debug/CMakeFiles/Main.dir/Catalog/source/MyDB_Table.cc.o"
+  "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/DatabaseTable/source/MyDB_PageReaderWriter.cc" "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/cmake-build-debug/CMakeFiles/Main.dir/DatabaseTable/source/MyDB_PageReaderWriter.cc.o"
+  "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/DatabaseTable/source/MyDB_TableReaderWriter.cc" "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/cmake-build-debug/CMakeFiles/Main.dir/DatabaseTable/source/MyDB_TableReaderWriter.cc.o"
+  "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/Record/source/MyDB_AttVal.cc" "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/cmake-build-debug/CMakeFiles/Main.dir/Record/source/MyDB_AttVal.cc.o"
+  "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/Record/source/MyDB_Record.cc" "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/cmake-build-debug/CMakeFiles/Main.dir/Record/source/MyDB_Record.cc.o"
+  "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/RecordTest/source/RecordQUnit.cc" "/Users/hanyiwang/Desktop/Rice/COMP530 Database Implementation/A2/Main/cmake-build-debug/CMakeFiles/Main.dir/RecordTest/source/RecordQUnit.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../BufferMgr/headers"
+  "../Catalog/headers"
+  "../DatabaseTable/headers"
+  "../Qunit"
+  "../Qunit/headers"
+  "../Record/headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ A2/Main/cmake-build-debug/CMakeFiles/Main.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Main.dir/BufferMgr/source/MyDB_BufferManager.cc.o"
+  "CMakeFiles/Main.dir/BufferMgr/source/MyDB_Page.cc.o"
+  "CMakeFiles/Main.dir/BufferTest/source/BufferQUnit.cc.o"
+  "CMakeFiles/Main.dir/Catalog/source/MyDB_Catalog.cc.o"
+  "CMakeFiles/Main.dir/Catalog/source/MyDB_Schema.cc.o"
+  "CMakeFiles/Main.dir/Catalog/source/MyDB_Table.cc.o"
+  "CMakeFiles/Main.dir/DatabaseTable/source/MyDB_PageReaderWriter.cc.o"
+  "CMakeFiles/Main.dir/DatabaseTable/source/MyDB_TableReaderWriter.cc.o"
+  "CMakeFiles/Main.dir/Record/source/MyDB_AttVal.cc.o"
+  "CMakeFiles/Main.dir/Record/source/MyDB_Record.cc.o"
+  "CMakeFiles/Main.dir/RecordTest/source/RecordQUnit.cc.o"
+  "Main"
+  "Main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
